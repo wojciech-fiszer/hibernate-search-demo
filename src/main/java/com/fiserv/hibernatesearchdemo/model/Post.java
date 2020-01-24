e8c5043328a0df3e6d7eb5d1f62759c681fe771d
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2020-01-24 12:33:46 +0100

    Polished test

--- src/main/java/com/fiserv/hibernatesearchdemo/model/Post.java
+++ src/main/java/com/fiserv/hibernatesearchdemo/model/Post.java
@@ -1,10 +1,12 @@
 package com.fiserv.hibernatesearchdemo.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Indexed;
+import lombok.*;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.ngram.EdgeNGramFilterFactory;
+import org.hibernate.search.annotations.*;
+import org.hibernate.search.annotations.Parameter;
 
 import javax.persistence.*;
 
@@ -12,6 +14,28 @@ import javax.persistence.*;
 @Indexed
 @Data
 @EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@AnalyzerDef(
+        name = "postContentIndexingAnalyzer",
+        tokenizer = @TokenizerDef(factory = WhitespaceTokenizerFactory.class),
+        filters = {
+                @TokenFilterDef(factory = ASCIIFoldingFilterFactory.class),
+                @TokenFilterDef(factory = LowerCaseFilterFactory.class),
+                @TokenFilterDef(
+                        factory = EdgeNGramFilterFactory.class,
+                        params = {
+                                @Parameter(name = "minGramSize", value = "1"),
+                                @Parameter(name = "maxGramSize", value = "10")
+                        }
+                )
+        })
+@AnalyzerDef(
+        name = "postContentQueryingAnalyzer",
+        tokenizer = @TokenizerDef(factory = WhitespaceTokenizerFactory.class),
+        filters = {
+                @TokenFilterDef(factory = ASCIIFoldingFilterFactory.class),
+                @TokenFilterDef(factory = LowerCaseFilterFactory.class)
+        }
+)
 public class Post {
 
     @Id
@@ -19,11 +43,16 @@ public class Post {
     @EqualsAndHashCode.Include
     private Long id;
 
-    @Field
+    @Field(name = Fields.CONTENT, analyzer = @Analyzer(definition = "postContentIndexingAnalyzer"))
     private String content;
 
     @ManyToOne
     @JoinColumn
     @ToString.Exclude
     private User user;
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public class Fields {
+        public static final String CONTENT = "content";
+    }
 }
--- src/test/java/com/fiserv/hibernatesearchdemo/HibernateSearchDemoApplicationTests.java
+++ src/test/java/com/fiserv/hibernatesearchdemo/HibernateSearchDemoApplicationTests.java
@@ -2,10 +2,9 @@ package com.fiserv.hibernatesearchdemo;
 
 import com.fiserv.hibernatesearchdemo.model.Post;
 import com.fiserv.hibernatesearchdemo.repository.PostRepository;
-import com.fiserv.hibernatesearchdemo.repository.UserRepository;
 import org.apache.lucene.search.Query;
-import org.hibernate.search.jpa.Search;
 import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,22 +25,17 @@ class HibernateSearchDemoApplicationTests {
     private PostRepository postRepository;
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
     private TransactionTemplate transactionTemplate;
 
     @Autowired
     private EntityManager entityManager;
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
+    @SuppressWarnings("unchecked")
     void indexPosts() {
         Post post = new Post();
-        post.setContent("content");
+        String oldContent = "content";
+        post.setContent(oldContent);
 
         transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
         transactionTemplate.execute(transactionStatus -> postRepository.save(post));
@@ -49,17 +43,21 @@ class HibernateSearchDemoApplicationTests {
         FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
         QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory().buildQueryBuilder()
                 .forEntity(Post.class)
+                .overridesForField(Post.Fields.CONTENT, "postContentQueryingAnalyzer")
                 .get();
 
-        Query query = queryBuilder.keyword().onField("content").matching("content").createQuery();
-        List resultList = fullTextEntityManager.createFullTextQuery(query, Post.class).getResultList();
+        Query query = queryBuilder.keyword().onField(Post.Fields.CONTENT).matching(oldContent).createQuery();
+        List<Post> resultList = fullTextEntityManager.createFullTextQuery(query, Post.class).getResultList();
         assertEquals(1, resultList.size());
 
-        post.setContent("updated");
+        String updatedContent = "updated";
+        post.setContent(updatedContent);
         transactionTemplate.execute(transactionStatus -> postRepository.save(post));
-        List queryForOldResultList = fullTextEntityManager.createFullTextQuery(query, Post.class).getResultList();
-        Query queryForUpdated = queryBuilder.keyword().onField("content").matching("updated").createQuery();
-        List queryForUpdatedResultList = fullTextEntityManager.createFullTextQuery(queryForUpdated, Post.class).getResultList();
+        List<Post> queryForOldResultList = fullTextEntityManager.createFullTextQuery(query, Post.class).getResultList();
+        Query queryForUpdated = queryBuilder.keyword().onField(Post.Fields.CONTENT).matching(updatedContent)
+                .createQuery();
+        List<Post> queryForUpdatedResultList = fullTextEntityManager.createFullTextQuery(queryForUpdated, Post.class)
+                .getResultList();
         assertTrue(queryForOldResultList.isEmpty());
         assertEquals(1, queryForUpdatedResultList.size());
     }
